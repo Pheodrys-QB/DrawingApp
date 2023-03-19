d0d4dc282369f9edf492d5d866b393bc688cd6cf
LeThuyNga1606 <lethuynga1662002@gmail.com>
2023-03-19 21:02:18 +0700

    paintapp

--- app/src/main/java/com/example/paintapp/MainActivity.java
+++ app/src/main/java/com/example/paintapp/MainActivity.java
@@ -8,23 +8,25 @@ import android.content.DialogInterface;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.MotionEvent;
 import android.view.ScaleGestureDetector;
 import android.view.View;
 import android.graphics.Path;
-import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
-import static com.example.paintapp.display.colorlist;
+import static com.example.paintapp.display.bitmap;
 import static com.example.paintapp.display.current_brush;
-import static com.example.paintapp.display.pathlist;
+import static com.example.paintapp.display.drawPaint;
+import static com.example.paintapp.display.drawPath;
+
+import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
     public static Path path = new Path();
     public static Paint brush = new Paint();
-
-    Button newPaint;
-
     View imgView;
     View motionLayer;
     float offsetX, offsetY;
@@ -67,87 +69,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-//        imgView = (View) findViewById(R.id.canvasLayer);
-//        motionLayer = (View) findViewById(R.id.motionLayer);
-//        scaleDetector = new ScaleGestureDetector(motionLayer.getContext(), new ScaleListener());
-//        newPaint = (Button) findViewById(R.id.newPaint);
-//
-//        newPaint.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                if (isGesture) {
-//                    isGesture = false;
-//                    motionLayer.setOnTouchListener(null);
-//                    motionLayer.setClickable(false);
-//
-//                } else {
-//                    isGesture = true;
-//                    motionLayer.setOnTouchListener(new View.OnTouchListener() {
-//                        @Override
-//                        public boolean onTouch(View view, MotionEvent motionEvent) {
-//                            scaleDetector.onTouchEvent(motionEvent);
-//
-//                                switch (motionEvent.getAction()) {
-//                                    case MotionEvent.ACTION_DOWN:
-//                                        refX = motionEvent.getX();
-//                                        refY = motionEvent.getY();
-//                                        break;
-//                                    case MotionEvent.ACTION_MOVE:
-//                                        float nX = motionEvent.getX();
-//                                        float nY = motionEvent.getY();
-//
-//                                        offsetX += nX - refX;
-//                                        offsetY += nY - refY;
-//
-//                                        refX = nX;
-//                                        refY = nY;
-//
-//                                        imgView.setTranslationX(offsetX);
-//                                        imgView.setTranslationY(offsetY);
-//                                        imgView.invalidate();
-//                                }
-//
-//
-//                            return true;
-//                        }
-//
-//
-//                    });
-//                }
-//            }
-//        });
     }
 
     public void redColor(View v) {
-        brush.setColor(Color.RED);
-        currentColor(brush.getColor());
-    }
 
-    public void yellowColor(View v) {
-        brush.setColor(Color.YELLOW);
-        currentColor(brush.getColor());
-    }
-
-    public void blueColor(View v) {
-        brush.setColor(Color.BLUE);
-        currentColor(brush.getColor());
-    }
-
-    public void teal_200Color(View v) {
-        brush.setColor(Color.MAGENTA);
-        currentColor(brush.getColor());
     }
 
     public void pencil(View v) {
-        brush.setColor(Color.BLACK);
-        currentColor(brush.getColor());
+        drawPaint.setColor(Color.BLACK);
+        currentColor(drawPaint.getColor());
     }
 
     public void eraser(View v) {
-        pathlist.clear();
-        colorlist.clear();
-        path.reset();
+       drawPath.reset();
     }
 
     public void currentColor(int c) {
@@ -161,9 +95,7 @@ public class MainActivity extends AppCompatActivity {
         newDialog.setMessage("Start new drawing (you will lose the current drawing)?");
         newDialog.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
             public void onClick(DialogInterface dialog, int which) {
-                pathlist.clear();
-                colorlist.clear();
-                path.reset();
+                drawPath.reset();
                 dialog.dismiss();
             }
         });
@@ -175,5 +107,113 @@ public class MainActivity extends AppCompatActivity {
         newDialog.show();
     }
 
+    public void dowloadPaint(View v) {
+        AlertDialog.Builder saveDialog = new AlertDialog.Builder(this);
+        saveDialog.setTitle("Save drawing");
+        saveDialog.setMessage("Save drawing to device Gallery?");
+        saveDialog.setPositiveButton("Yes", new DialogInterface.OnClickListener(){
+            public void onClick(DialogInterface dialog, int which){
+                imgView = (View) findViewById(R.id.drawView);
+                imgView.setDrawingCacheEnabled(true);
+                String imgSaved = MediaStore.Images.Media.insertImage(
+                        getContentResolver(), imgView.getDrawingCache(),
+                        UUID.randomUUID().toString()+".png", "drawing");
+                if(imgSaved!=null){
+                    Toast savedToast = Toast.makeText(getApplicationContext(),
+                            "Drawing saved to Gallery!", Toast.LENGTH_SHORT);
+                    savedToast.show();
+                }
+                else{
+                    Toast unsavedToast = Toast.makeText(getApplicationContext(),
+                            "Oops! Image could not be saved.", Toast.LENGTH_SHORT);
+                    unsavedToast.show();
+                }
+                imgView.destroyDrawingCache();
+            }
+        });
+        saveDialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener(){
+            public void onClick(DialogInterface dialog, int which){
+                dialog.cancel();
+            }
+        });
+        saveDialog.show();
+    }
 
+    public void zoomImg(View v) {
+        imgView = (View) findViewById(R.id.drawView);
+        motionLayer = (View) findViewById(R.id.motionLayer);
+        scaleDetector = new ScaleGestureDetector(motionLayer.getContext(), new ScaleListener());
+        if (isGesture) {
+            isGesture = false;
+            motionLayer.setOnTouchListener(null);
+            motionLayer.setClickable(false);
+
+        } else {
+            isGesture = true;
+            motionLayer.setOnTouchListener(new View.OnTouchListener() {
+                @Override
+                public boolean onTouch(View view, MotionEvent motionEvent) {
+                    scaleDetector.onTouchEvent(motionEvent);
+                    switch (motionEvent.getAction()) {
+                        case MotionEvent.ACTION_DOWN:
+                            refX = motionEvent.getX();
+                            refY = motionEvent.getY();
+                            break;
+                        case MotionEvent.ACTION_MOVE:
+                            float nX = motionEvent.getX();
+                            float nY = motionEvent.getY();
+
+                            offsetX += nX - refX;
+                            offsetY += nY - refY;
+
+                            refX = nX;
+                            refY = nY;
+
+                            imgView.setTranslationX(offsetX);
+                            imgView.setTranslationY(offsetY);
+                            imgView.invalidate();
+                    }
+                    return true;
+                }
+            });
+        }
+    }
+
+    public void openColorPicker(View v){
+        setContentView(R.layout.color_picker);
+        ImageView imgcolorpicker = (ImageView) findViewById(R.id.imgcolorpicker);
+        imgcolorpicker.setDrawingCacheEnabled(true);
+        imgcolorpicker.buildDrawingCache(true);
+        Button btn_ok = (Button)findViewById(R.id.btn_ok);
+        Button btn_close = (Button)findViewById(R.id.btn_close);
+        View viewColor = (View)findViewById(R.id.view_color);
+
+        imgcolorpicker.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                if (motionEvent.getAction() == MotionEvent.ACTION_DOWN || motionEvent.getAction() == MotionEvent.ACTION_MOVE) {
+                    bitmap = imgcolorpicker.getDrawingCache();
+                    int pixel = bitmap.getPixel((int)motionEvent.getX(), (int)motionEvent.getY());
+                    int r = Color.red(pixel);
+                    int g = Color.green(pixel);
+                    int b = Color.blue(pixel);
+                    viewColor.setBackgroundColor(Color.rgb(r, g, b));
+                    drawPaint.setColor(Color.rgb(r, g, b));
+                }
+                return true;
+            }
+        });
+
+        btn_ok.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        btn_close.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+        });
+    }
 }
--- app/src/main/java/com/example/paintapp/display.java
+++ app/src/main/java/com/example/paintapp/display.java
@@ -13,19 +13,17 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 
 import androidx.annotation.Nullable;
-import static com.example.paintapp.MainActivity.brush;
-import static com.example.paintapp.MainActivity.path;
 
 import java.util.ArrayList;
 
 public class display extends androidx.appcompat.widget.AppCompatImageView {
-    public static ArrayList<Path> pathlist = new ArrayList<>();
-    public static ArrayList<Integer> colorlist = new ArrayList<>();
     public ViewGroup.LayoutParams params;
+    public static Bitmap bitmap;
+    public static Canvas mcanvas;
 
-    Bitmap bitmap;
-    Canvas mcanvas;
-    public static int current_brush = Color.RED;
+    public static Path drawPath;
+    public static Paint drawPaint, canvasPaint;
+    public static int current_brush = Color.BLACK;
 
     public display(Context context){
         super(context);
@@ -43,14 +41,19 @@ public class display extends androidx.appcompat.widget.AppCompatImageView {
     }
 
     private void init(Context context){
-        brush.setAntiAlias(true);
-        brush.setColor(Color.RED);
-        brush.setStyle(Paint.Style.STROKE);
-        brush.setStrokeCap(Paint.Cap.ROUND);
-        brush.setStrokeJoin(Paint.Join.ROUND);
-        brush.setStrokeWidth(10f);
+        drawPath = new Path();
+        drawPaint = new Paint();
+
+        drawPaint.setColor(current_brush);
+
+        drawPaint.setAntiAlias(true);
+        drawPaint.setStrokeWidth(20);
+        drawPaint.setStyle(Paint.Style.STROKE);
+        drawPaint.setStrokeJoin(Paint.Join.ROUND);
+        drawPaint.setStrokeCap(Paint.Cap.ROUND);
         params = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
 
+        canvasPaint = new Paint(Paint.DITHER_FLAG);
 
         bitmap = Bitmap.createBitmap(100, 100, Bitmap.Config.ARGB_8888);
         mcanvas = new Canvas(bitmap);
@@ -59,35 +62,25 @@ public class display extends androidx.appcompat.widget.AppCompatImageView {
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-        float x = event.getX();
-        float y = event.getY();
-        switch (event.getAction()){
+        float touchX = event.getX();
+        float touchY = event.getY();
+        switch (event.getAction()) {
             case MotionEvent.ACTION_DOWN:
-                path.moveTo(x,y);
-                invalidate();
-                return true;
+                drawPath.moveTo(touchX, touchY);
+                break;
             case MotionEvent.ACTION_MOVE:
-                path.lineTo(x,y);
-                pathlist.add(path);
-                colorlist.add(current_brush);
-                invalidate();
-                return true;
-            case  MotionEvent.ACTION_UP:
-                pathlist.clear();
-                colorlist.clear();
-                path.reset();
-                return  true;
+                drawPath.lineTo(touchX, touchY);
+                break;
             default:
                 return false;
         }
+        invalidate();
+        return true;
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
-        for(int i = 0; i < pathlist.size(); i++){
-            brush.setColor(colorlist.get(i));
-            mcanvas.drawPath(pathlist.get(i), brush);
-            invalidate();
-        }
+        canvas.drawBitmap(bitmap, 0, 0, canvasPaint);
+        canvas.drawPath(drawPath, drawPaint);
     }
 }
