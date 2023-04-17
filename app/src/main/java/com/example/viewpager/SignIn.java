1ecbf45471a3fc48dd320ea48b28c5936d09f7ae
Quốc Bảo <81245741+Pheodrys-QB@users.noreply.github.com>
2023-04-17 10:14:17 +0700

    feat: add user  to database, update username in profile

--- app/src/main/java/com/example/viewpager/SignIn.java
+++ app/src/main/java/com/example/viewpager/SignIn.java
@@ -1,7 +1,6 @@
 package com.example.viewpager;
 
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -17,29 +16,34 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class SignIn extends AppCompatActivity {
     private Activity act;
     TextView switchScreen;
     Button acceptInput;
-    EditText username, password, confirmPassword;
+    EditText email, password, confirmPassword, username;
     private FirebaseAuth mAuth;
 
     private void innitLogIn(){
         setContentView(R.layout.fragment_login);
-        username = findViewById(R.id.lgUsername);
+        email = findViewById(R.id.lgEmail);
         password = findViewById(R.id.lgPassword);
         switchScreen = findViewById(R.id.lgSwitchScreen);
         acceptInput = findViewById(R.id.loginBtn);
         acceptInput.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String email = username.getText().toString();
+                String _email = email.getText().toString();
                 String pass = password.getText().toString();
-                if (email.equals("") || pass.equals("")) {
+                if (_email.equals("") || pass.equals("")) {
                     return;
                 }
-                mAuth.signInWithEmailAndPassword(email, pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                mAuth.signInWithEmailAndPassword(_email, pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
@@ -65,24 +69,33 @@ public class SignIn extends AppCompatActivity {
 
     private void innitRegister(){
         setContentView(R.layout.fragment_register);
-        username = findViewById(R.id.rgEmail);
+        email = findViewById(R.id.rgEmail);
         password = findViewById(R.id.rgPassword);
         confirmPassword = findViewById(R.id.rgConfirmPassword);
         acceptInput = findViewById(R.id.registerBtn);
+        username = findViewById(R.id.rgUsername);
         acceptInput.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String email = username.getText().toString();
+                String _email = email.getText().toString();
                 String pass = password.getText().toString();
                 String cfmPass = confirmPassword.getText().toString();
-                if (email.equals("") || pass.equals("") || cfmPass.equals("") || !pass.equals(cfmPass)) {
+                String name = username.getText().toString();
+                if (_email.equals("") || pass.equals("") || cfmPass.equals("") || !pass.equals(cfmPass) || name.equals("")) {
                     return;
                 }
-                mAuth.createUserWithEmailAndPassword(email, pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                mAuth.createUserWithEmailAndPassword(_email, pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
-                            // Sign in success, update UI with the signed-in user's information
+                            // Sign in success, update UI with the signed-in user's information.
+                            FirebaseUser mUser = mAuth.getCurrentUser();
+                            FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+                            Map<String, Object> data = new HashMap<>();
+                            data.put("username", name);
+                            db.collection("users").document(mUser.getUid()).set(data);
+
                             startActivity(new Intent(act, MainActivity.class));
                             finish();
                         } else {
--- app/src/main/java/com/example/viewpager/fragment/ProfileFragment.java
+++ app/src/main/java/com/example/viewpager/fragment/ProfileFragment.java
@@ -5,12 +5,20 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import com.example.viewpager.MainActivity;
 import com.example.viewpager.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 //import com.google.firebase.auth.FirebaseAuth;
 //import com.google.firebase.auth.FirebaseUser;
 
@@ -28,6 +36,8 @@ public class ProfileFragment extends Fragment {
     private String mParam2;
 
     private FirebaseAuth mAuth;
+    private FirebaseUser mUser;
+    private String username = "";
 
 
     public ProfileFragment() {
@@ -52,6 +62,21 @@ public class ProfileFragment extends Fragment {
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
         mAuth = FirebaseAuth.getInstance();
+        mUser = mAuth.getCurrentUser();
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        DocumentReference docRef = db.collection("users").document(mUser.getUid());
+        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists()) {
+                        username = document.get("username").toString();
+
+                    }
+                }
+            }
+        });
     }
 
     @Override
@@ -59,14 +84,16 @@ public class ProfileFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View curView = inflater.inflate(R.layout.fragment_profile, container, false);
+        TextView textView = curView.findViewById(R.id.profileUsername);
+        textView.setText(username);
         curView.findViewById(R.id.logoutBtn).setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        mAuth.signOut();
-                        startActivity(new Intent(getContext(), MainActivity.class));
-                        getActivity().finish();
-                    }
-                }
+                                                                    @Override
+                                                                    public void onClick(View view) {
+                                                                        mAuth.signOut();
+                                                                        startActivity(new Intent(getContext(), MainActivity.class));
+                                                                        getActivity().finish();
+                                                                    }
+                                                                }
         );
 
         return curView;
