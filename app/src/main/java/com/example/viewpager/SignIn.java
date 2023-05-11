6e8a2fa24655d0a0903344126df64d143dd87be1
Quốc Bảo <81245741+Pheodrys-QB@users.noreply.github.com>
2023-05-11 11:38:26 +0700

    update login + registration UI

--- app/src/main/java/com/example/viewpager/SignIn.java
+++ app/src/main/java/com/example/viewpager/SignIn.java
@@ -15,6 +15,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -27,11 +28,85 @@ public class SignIn extends AppCompatActivity {
     private Activity act;
     TextView switchScreen;
     Button acceptInput;
-    EditText email, password, confirmPassword, username;
-    private FirebaseAuth mAuth;
 
-    private void innitLogIn(){
+    TextInputLayout email, password, confirmPassword, username;
+    private FirebaseAuth mAuth;
+    private boolean isLogIn = true;
+
+    private boolean validateEmail() {
+        String val = email.getEditText().getText().toString();
+        String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+
+        if (val.isEmpty()) {
+            email.setError("Field must not be empty");
+            return false;
+        } else if (!val.matches(emailPattern)) {
+            email.setError("Invalid email address");
+            return false;
+        } else {
+            email.setError(null);
+            email.setErrorEnabled(false);
+            return true;
+        }
+    }
+
+    private boolean validatePassword() {
+        String val = password.getEditText().getText().toString();
+        String passwordVal = "^" +
+                //"(?=.*[0-9])" +         //at least 1 digit
+                //"(?=.*[a-z])" +         //at least 1 lower case letter
+                //"(?=.*[A-Z])" +         //at least 1 upper case letter
+                "(?=.*[a-zA-Z])" +      //any letter
+//                "(?=.*[@#$%^&+=])" +    //at least 1 special character
+                "(?=\\S+$)" +           //no white spaces
+                ".{6,}" +               //at least 6 characters
+                "$";
+        if (val.isEmpty()) {
+            password.setError("Field must not be empty");
+            return false;
+        } else if (!val.matches(passwordVal)) {
+            password.setError("Password is too weak");
+            return false;
+        } else {
+            password.setError(null);
+            password.setErrorEnabled(false);
+            return true;
+        }
+    }
+
+    private boolean validateReconfirmPassword() {
+        String val = confirmPassword.getEditText().getText().toString();
+        String tPassword = email.getEditText().getText().toString();
+        if (val.isEmpty()) {
+            confirmPassword.setError("Field must not be empty");
+            return false;
+        } else if (!val.equals(tPassword)) {
+            confirmPassword.setError("Must match password");
+            return false;
+        } else {
+            confirmPassword.setError(null);
+            confirmPassword.setErrorEnabled(false);
+            return true;
+        }
+    }
+
+    private boolean validateUsername() {
+        String val = username.getEditText().getText().toString();
+
+        if (val.isEmpty()) {
+            username.setError("Field must not be empty");
+            return false;
+        } else {
+            username.setError(null);
+            username.setErrorEnabled(false);
+            return true;
+        }
+
+    }
+
+    private void innitLogIn() {
         setContentView(R.layout.fragment_login);
+        isLogIn = true;
         email = findViewById(R.id.lgEmail);
         password = findViewById(R.id.lgPassword);
         switchScreen = findViewById(R.id.lgSwitchScreen);
@@ -39,9 +114,9 @@ public class SignIn extends AppCompatActivity {
         acceptInput.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String _email = email.getText().toString();
-                String pass = password.getText().toString();
-                if (_email.equals("") || pass.equals("")) {
+                String _email = email.getEditText().getText().toString();
+                String pass = password.getEditText().getText().toString();
+                if (_email.isEmpty() || pass.isEmpty()) {
                     return;
                 }
                 mAuth.signInWithEmailAndPassword(_email, pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
@@ -68,7 +143,7 @@ public class SignIn extends AppCompatActivity {
 
     }
 
-    private void innitRegister(){
+    private void innitRegister() {
         setContentView(R.layout.fragment_register);
         email = findViewById(R.id.rgEmail);
         password = findViewById(R.id.rgPassword);
@@ -78,13 +153,13 @@ public class SignIn extends AppCompatActivity {
         acceptInput.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String _email = email.getText().toString();
-                String pass = password.getText().toString();
-                String cfmPass = confirmPassword.getText().toString();
-                String name = username.getText().toString();
-                if (_email.equals("") || pass.equals("") || cfmPass.equals("") || !pass.equals(cfmPass) || name.equals("")) {
+                if (!validateEmail() | !validateUsername() | !validatePassword() | !validateReconfirmPassword()) {
                     return;
                 }
+                String _email = email.getEditText().getText().toString();
+                String pass = password.getEditText().getText().toString();
+                String cfmPass = confirmPassword.getEditText().getText().toString();
+                String name = username.getEditText().getText().toString();
                 mAuth.createUserWithEmailAndPassword(_email, pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
@@ -101,7 +176,7 @@ public class SignIn extends AppCompatActivity {
                             finish();
                         } else {
                             // If sign in fails, display a message to the user.
-                            Toast.makeText(getApplicationContext(),"Password must be at least 6 characters", Toast.LENGTH_LONG).show();
+                            Toast.makeText(getApplicationContext(), "Password must be at least 6 characters", Toast.LENGTH_LONG).show();
                         }
                     }
                 });
